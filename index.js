7f697b907b0933d86c9b99750e0916238ac0c486
Nelly Sugu <jonly03@gmail.com>
2022-03-19 22:15:19 -0400

    initial commit with cors enabled

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const server = express(); // server CAN'T LISTEN TO REQUESTS
+
+server.use(cors());
+
+const port = 3000;
+
+server.get("/inflation", (req, res) => {
+  const { targetValue, startYear } = req.query;
+
+  const inflationAPIUrl = `https://inflation-api.herokuapp.com/api?value=${targetValue}&year=${startYear}`;
+
+  fetch(inflationAPIUrl)
+    .then((result) => result.json())
+    .then((data) => res.send(data));
+});
+
+server.listen(port, () => {
+  console.log("Server listening");
+});
